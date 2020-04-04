1e74d96e4d4ce77e33dded77bf4dcf9319875f8d
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-04-04 22:45:17 +1100

    scoreboard + scorebar

--- db/schema.rb
+++ db/schema.rb
@@ -91,15 +91,6 @@ ActiveRecord::Schema.define(version: 2020_04_04_070529) do
     t.index ["user_id"], name: "index_user_groups_on_user_id"
   end
 
-  create_table "user_sentences", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "sentence_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["sentence_id"], name: "index_user_sentences_on_sentence_id"
-    t.index ["user_id"], name: "index_user_sentences_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -109,6 +100,7 @@ ActiveRecord::Schema.define(version: 2020_04_04_070529) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.bigint "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
