49f6cb741ad38c1e2f60ee66eb2655887e208ca6
Wesley Fletcher <wes.j.fletcher@gmail.com>
2020-04-04 23:15:21 +1100

    added court page

--- db/schema.rb
+++ db/schema.rb
@@ -91,15 +91,6 @@ ActiveRecord::Schema.define(version: 2020_04_04_070529) do
     t.index ["user_id"], name: "index_user_groups_on_user_id"
   end
 
-  create_table "user_sentences", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "sentence_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["sentence_id"], name: "index_user_sentences_on_sentence_id"
-    t.index ["user_id"], name: "index_user_sentences_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
