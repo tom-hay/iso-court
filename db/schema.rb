62775cee51c8e68cb86519510c49773b7ffa9c27
Cameron Melville <cam.t.melville@gmail.com>
2020-04-04 12:39:59 +1100

    run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_004923) do
+ActiveRecord::Schema.define(version: 2020_04_04_012645) do
+
+  create_table "commitments", force: :cascade do |t|
+    t.string "type"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
