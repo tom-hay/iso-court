eeec9f8a7dbe740d2b4fc0d0507d7f76d7b3b217
Tom Haywood <tom.a.haywood@gmail.com>
2020-04-05 12:26:42 +1000

    made change to environments

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+config.assets.initialize_on_precompile = false
+
 require_relative 'boot'
 
 require "rails"
@@ -20,7 +22,6 @@ Bundler.require(*Rails.groups)
 module IsoCourt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.assets.initialize_on_precompile = false
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
