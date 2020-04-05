2d3af3a1ebe8d8cff057c23841c958a320fa5a67
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-04-05 12:00:11 +1000

    cleaned up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,23 @@
 Rails.application.routes.draw do
 
-  get 'charges/new'
-  get 'charges/create'
+  root to: "groups#index"
+
+  get 'groups/:id/court', to: 'groups#court', as: :court
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :groups do
+    resources :charges, only: [:new, :create] do
+      resources :sentences, only: [:new, :create]
+    end
+  end
+
   get 'charges/destroy'
+  get 'sentences/destroy'
+
+  # Current_user will commit
   get 'commitments/new'
   get 'commitments/create'
   get 'commitments/destroy'
-
-  get 'sentences/new'
-  get 'sentences/create'
-  get 'sentences/destroy'
-
-  get 'groups/:id/court', to: 'groups#court', as: :court
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/create'
-  get 'groups/edit'
-  get 'groups/destroy'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
-
-  resources :groups, only: [:show, :destroy] do
-    resources :charges, only: [:new, :create]
-  end
 end
